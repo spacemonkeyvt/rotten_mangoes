1c39a8df955722e0021137e1e74e075b32d7f968
Vishal Tiwari <vishal.tiwari.09@gmail.com>
2016-09-01 17:13:43 -0400

    Filling out CRUD function details (untested)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'movies/index'
 
-  get 'movies/show'
+  resources :movies
 
-  get 'movies/new'
-
-  get 'movies/edit'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
