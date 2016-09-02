c90ceaff9d5c4021415b141a966a606f0c0334e5
Vishal Tiwari <vishal.tiwari.09@gmail.com>
2016-09-02 13:33:02 -0400

    added some helper methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -22,6 +22,10 @@ class Movie < ApplicationRecord
 
   validate :release_date_is_in_the_past
 
+  def review_average
+    reviews.sum(:rating_out_of_ten)/reviews.size
+  end
+
   protected
 
   def release_date_is_in_the_past
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,8 @@ class User < ApplicationRecord
   validates :password,
     length: { in: 6..20 }, on: :create
 
+  def full_name
+    "#{firstname} #{lastname}"
+  end
+
 end
